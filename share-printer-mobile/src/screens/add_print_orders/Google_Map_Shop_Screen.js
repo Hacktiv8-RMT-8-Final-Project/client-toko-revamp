a5518d7a73d86fe1aa918e6c68a0d516fb6750a7
CLorenzoH <christ.lorenzo.h@gmail.com>
2021-03-21 17:35:51 +0700

    layout google map, mines picker

--- share-printer-mobile/src/screens/add_print_orders/Google_Map_Shop_Screen.js
+++ share-printer-mobile/src/screens/add_print_orders/Google_Map_Shop_Screen.js
@@ -1,27 +1,60 @@
 import React from "react"
-import { TouchableOpacity, StyleSheet, Text, View } from "react-native"
+import { TouchableOpacity, StyleSheet, Text, View, Dimensions } from "react-native"
+
+import MapView from "react-native-maps"
 
 function Google_Map_Shop_Screen(props) {
   const confirm_choose_shop = () => {
     props.navigation.navigate("Shop Profile")
   }
   return (
-    <View style={styles.container}>
-      <Text>Choose shop first - Google Map Here</Text>
-      <TouchableOpacity onPress={confirm_choose_shop} style={styles.button}>
-        <Text style={styles.button_text}>Confirm Shop</Text>
-      </TouchableOpacity>
-      <Text>Google_Map_Shop_Screen!</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.container_map}>
+          <MapView style={styles.map} />
+        </View>
+        <View style={styles.container_shop}>
+          <TouchableOpacity onPress={confirm_choose_shop} style={styles.registerBtn}>
+            <Text style={styles.button_text}>Confirm Shop</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
+  container_map: {
+    flex: 3,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container_shop: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  registerBtn: {
+    width: "80%",
+    backgroundColor: "white",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    marginBottom: 10,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+
   button: {
     display: "flex",
     height: 40,
