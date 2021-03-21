68e3a9dd5ec331d8386478c9e685eb3097eee55b
CLorenzoH <christ.lorenzo.h@gmail.com>
2021-03-21 11:10:23 +0700

    dashboard navigation

---
+++ share-printer-mobile/src/screens/dashboards/Profile_Settings_Screen.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+function Profile_Settings_Screen() {
+  return (
+    <View style={styles.container}>
+      <Text>Profile_Settings_Screen!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+})
+
+export default Profile_Settings_Screen
---
+++ share-printer-mobile/src/screens/dashboards/index.js
@@ -0,0 +1,6 @@
+export { default as Add_Order_Screen } from "./Add_Order_Screen.js"
+
+export { default as Current_Orders_Screen } from "./Current_Orders_Screen.js"
+export { default as Transaction_History_Screen } from "./Transaction_History_Screen.js"
+
+export { default as Profile_Settings_Screen } from "./Profile_Settings_Screen.js"
