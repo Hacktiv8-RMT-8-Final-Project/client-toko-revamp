eba37ccf12c682181322cd1307afe3956cc0fa7d
CLorenzoH <christ.lorenzo.h@gmail.com>
2021-03-22 17:13:58 +0700

    reupload for merge

--- share-printer-mobile/src/screens/Login_Screen.js
+++ share-printer-mobile/src/screens/Login_Screen.js
@@ -22,22 +22,25 @@ function Login_Screen(props) {
   const go_to_register_screen = () => {
     props.navigation.navigate("Register")
   }
+
   const go_to_dashboard_screen = () => {
-    props.navigation.navigate("Dashboard")
-    // axios({
-    //   method: "POST",
-    //   url: "http://192.168.0.102:3000/user/login",
-    //   data: { email, password },
-    // })
-    //   .then(({ data }) => {
-    //     // console.log(data.access_token);
-    //     AsyncStorage.setItem("access_token", JSON.stringify(data.access_token))
-    //     props.navigation.navigate("Dashboard")
-    //   })
-    //   .catch((err) => {
-    //     alert(err)
-    //     console.log(err)
-    //   })
+    // ! comment for login with access_token
+    // props.navigation.navigate("Dashboard")
+
+    axios({
+      method: "POST",
+      url: "http://192.168.0.102:3000/user/login",
+      data: { email, password },
+    })
+      .then(({ data }) => {
+        // console.log(data.access_token);
+        AsyncStorage.setItem("access_token", JSON.stringify(data.access_token))
+        props.navigation.navigate("Dashboard")
+      })
+      .catch((err) => {
+        alert(err)
+        console.log(err)
+      })
   }
 
   AsyncStorage.clear()
